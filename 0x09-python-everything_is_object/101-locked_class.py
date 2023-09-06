a0c39d5121e5dc36863e0f0e94add96c5d782f49
Natnael12119 <nhattydeveloper@gmail.com>
2023-09-06 11:00:16 -0700

    Bug expected to be fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 class LockedClass:
-    __slots__ = ["first_name"]
+    def __init__(self):
+        pass
+
+    def __setattr__(self, name, value):
+        if name == "first_name":
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError(f"Can't create attribute {name}")
