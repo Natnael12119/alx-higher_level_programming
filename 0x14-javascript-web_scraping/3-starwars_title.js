4d335a7cf6b15e70fa5ddf0b4e975b9c22c843eb
Natnael12119 <nhattydeveloper@gmail.com>
2024-02-20 13:24:37 -0800

    Debugging

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
+
 const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-const url = 'https://swapi-api.alx-tools.com/api/films/:id' +  process.argv[2];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
-    console.log('code:', response.statusCode);
+    console.log('Error code: ' + response.statusCode);
   }
 });
